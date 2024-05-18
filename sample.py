e9a8d926fcba0daf60e260dcd381911ea3a99135
sofia-adam <sofiaadam916@gmail.com>
2024-05-18 12:00:36 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,24 @@
+a = "Global Variable"
+def sample(b):
+    print("Hello",b)
+    d = "python"
+
+    def sample1():
+        a = "Rajesh"
+        print("world")
+        print(a)
+    
+    sample1()
+    print(d)
+c = "World War hero"
+sample(c)
+print(a)
+
+def fun():
+    pass
+
+s = "jhdgudhidjdahuhiuhijoiih"
+for i in s:
+    print(i,end="")
+
+
